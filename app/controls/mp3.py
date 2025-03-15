e7bd3d0dc4036e1787c06f57216f631f4989212f
gitviini <gvinicius105@gmail.com>
2025-03-14 23:31:40 -0300

    download in .mp4 format add

--- app/controls/mp3.py
+++ app/controls/mp3.py
@@ -1,20 +1,28 @@
 from __future__ import unicode_literals
 import yt_dlp as youtube_dl
 
+mode = {
+    "mp4" : {
+        'format': 'best[ext=mp4]', 
+        'force_keyframes_at_cuts': True,
+    },
+    "mp3" : {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp4',
+            'preferredquality': '192',
+        }],
+    }
+}
+
 def progress():
     print("progress")
 
 try:
     # baixa yt_url para o mesmo diretório no qual o script é executado
     def download_audio(yt_url):
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-        }
+        ydl_opts = mode["mp3"]
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([yt_url])
 except Exception as erro:
